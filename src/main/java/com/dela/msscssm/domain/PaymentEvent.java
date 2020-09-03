1ef9aa0fb16361b88ee1e1d7666744b4ec491323
delalic <delalicharis@gmail.com>
2020-09-03 21:33:45 +0200

    change directory names, add events and states enums

---
+++ src/main/java/com/dela/msscssm/domain/PaymentEvent.java
@@ -0,0 +1,6 @@
+package com.dela.msscssm.domain;
+
+public enum PaymentEvent {
+    PRE_AUTHORIZE, PRE_AUTHORIZE_APPROVE, PRE_AUTHORIZE_DECLINE,
+    AUTHORIZE, AUTHORIZE_APPROVE, AUTHORIZE_DECLINE
+}
